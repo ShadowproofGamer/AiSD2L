81bc10b5241fe89093527dd927617a35c63748aa
ShadowproofGamer <57487079+ShadowproofGamer@users.noreply.github.com>
2022-03-16 22:20:41 +0100

    v2

--- src/Main.java
+++ src/Main.java
@@ -19,30 +19,50 @@ public class Main {
         //
         //}
 
-        Pracownik[] tab = new Pracownik[5];
+        Pracownik[] tab = wczytaj(new Pracownik[50], 0);
+        wyswietl(tab);
 
 
     }
-    public void wczytaj(Pracownik[] tab, int start){
+    public static Pracownik[] wczytaj(Pracownik[] tab, int start){
         boolean toContinue = true;
         Scanner scan = new Scanner(System.in);
         while(toContinue){
+
+            System.out.println("Wpisz typ (0=etat, 1=godziny):");
+            String typ = scan.next();
             System.out.println("Wpisz nazwisko:");
             String nazwisko = scan.next();
             System.out.println("Wpisz imie:");
             String imie = scan.next();
             System.out.println("Wpisz pesel:");
             String pesel = scan.next();
-            System.out.println("Wpisz stanowisko");
+            System.out.println("Wpisz stanowisko:");
             String stanowisko = scan.next();
             System.out.println("Wpisz staż (lata):");
             int staz = scan.nextInt();
-            System.out.println("Wpisz etat");
+            System.out.println("Wpisz etat/liczbe godzin:");
             double etat = scan.nextDouble();
             System.out.println("Wpisz stawkę:");
             double stawka = scan.nextDouble();
+            if (typ.equals("0")||typ.equals("etat")){
+                tab[start] = new PracownikEtatowy(nazwisko,imie,Long.parseLong(pesel),stanowisko,staz,etat,stawka);
+                start++;
+            }else if (typ.equals("1")||typ.equals("godziny")){
+                tab[start] = new PracownikGodzinowy(nazwisko,imie,Long.parseLong(pesel),stanowisko,staz, (int) etat,stawka);
+                start++;
+            }
+            System.out.println("Czy chcesz kontynuować? (0=nie  1=tak):");
+            if (scan.next().equals("0"))toContinue=false;
 
         }
-
+        return tab;
+    }
+    public static void wyswietl(Pracownik[] tab){
+        System.out.printf("%s, %s, %s, %s, %s, %s", "nazwisko", "imie", "pesel", "stanowisko", "staz", "pensja");
+        for (Pracownik p :
+                tab) {
+            System.out.println(p);
+        }
     }
 }
